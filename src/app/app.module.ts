08027761b2f18b575c91ad4cabf26ba4a356c339
Danail Videv <danailvidev@gmail.com>
2020-08-12 12:54:48 +0300

    add core module with httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -9,6 +8,7 @@ import en from '@angular/common/locales/en';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 
+import { CoreModule } from './core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { IconsProviderModule } from './icons-provider.module';
@@ -20,11 +20,11 @@ registerLocaleData(en);
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule,
     IconsProviderModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US },
