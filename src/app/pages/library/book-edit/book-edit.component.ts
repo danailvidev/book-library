058b44912ecdcd599b2250879cd8abe1566ac064
Danail Videv <danailvidev@gmail.com>
2020-08-12 16:33:54 +0300

    add book edit component

--- src/app/pages/library/book-edit/book-edit.component.ts
+++ src/app/pages/library/book-edit/book-edit.component.ts
@@ -1,12 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NzDrawerRef } from 'ng-zorro-antd/drawer';
+
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { Book } from '@shared/library';
+import { LibraryService } from '@shared/library/library.service';
+
+export enum MessageType {
+  SUCCESS = 'success',
+  ERROR = 'error',
+  WARNING = 'warning'
+}
+
 @Component({
-    selector: 'ch-book-edit',
-    templateUrl: './book-edit.component.html',
-    styleUrls: ['./book-edit.component.scss']
+  selector: 'ch-book-edit',
+  templateUrl: './book-edit.component.html',
+  styleUrls: ['./book-edit.component.scss']
 })
 export class BookEditComponent implements OnInit {
-    constructor() { }
+  public request: Book;
 
-    ngOnInit(): void { }
+  constructor(
+    public svc: LibraryService,
+    protected drawerRef: NzDrawerRef<any>,
+    protected message: NzMessageService
+  ) {}
+
+  ngOnInit(): void {}
+
+  public onCancel(event: any) {
+    this.drawerRef.close('onCancel');
+  }
+
+  public onDelete(request: Book) {
+    if (request.id) {
+      this.svc.delete(request.id).subscribe(
+        (res) => {
+          this.drawerRef.close('onDelete');
+          this.createMessage(MessageType.SUCCESS, 'Successfully Deleted');
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error)
+            this.message.error(err.error.error, {
+              nzDuration: 5000,
+              nzPauseOnHover: true
+            });
+        }
+      );
+    }
+  }
+
+  public submit(request: Book) {
+    if (request) {
+      this.svc
+        .update(request.id, request)
+        .pipe(
+          catchError((err) => {
+            const ERROR = err.error.error || new Error();
+            this.createMessage(MessageType.ERROR, ERROR);
+            return throwError(err);
+          })
+        )
+        .subscribe((res: Book) => {
+          if (res) {
+            this.drawerRef.close(res);
+            this.createMessage(MessageType.SUCCESS, 'Successfully Updated');
+          }
+        });
+    }
+  }
+
+  public createMessage(type: MessageType, message: string) {
+    switch (type) {
+      case MessageType.SUCCESS:
+        this.message.success(message);
+        break;
+      case MessageType.ERROR:
+        this.message.error(message);
+        break;
+      case MessageType.WARNING:
+        this.message.warning(message);
+        break;
+      default:
+        break;
+    }
+  }
 }
--- src/app/pages/library/book-edit/book-edit.module.ts
+++ src/app/pages/library/book-edit/book-edit.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookEditComponent } from './book-edit.component';
-// TODO import form module
+import { BookFormModule } from '../book-form/book-form.module';
 
 @NgModule({
   declarations: [BookEditComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, BookFormModule]
 })
 export class BookEditModule {}
